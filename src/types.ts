a9f3c724bf7f359dc08470a1de55bf6623cdac6f
aster-mnch <mnch.mai@gmail.com>
2024-11-14 17:06:56 +0900

    docs: add JSDocs to types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,15 @@
 export type DownloadOption = {
+  /** The directory to which the repository will be downloaded. */
   dir?: string;
+  /** If `true`, allows downloading into an existing directory. */
   force?: boolean;
+  /** Authorization token for accessing private repositories. */
   auth?: string;
 };
 
 export type DownloadResult = {
+  /** The path where the repository was extracted. */
   source: string;
+  /** The identifier of the downloaded repository. */
   dir: string;
 };
